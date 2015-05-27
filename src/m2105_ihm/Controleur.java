ad7846854a5ff212c4318545be1b8e12ddcabd52
Armand <Armand@Armand-PP>
2015-05-27 10:55:45 +0200

    Creation projet

---
+++ src/m2105_ihm/Controleur.java
@@ -0,0 +1,172 @@
+/*
+ * Module 2105 : module IHM : Carnet d'adresse
+ */
+package m2105_ihm;
+
+import m2105_ihm.nf.Contact;
+import m2105_ihm.nf.GroupeContacts;
+import m2105_ihm.nf.NoyauFonctionnel;
+import m2105_ihm.nf.Evenement;
+
+import m2105_ihm.ui.CarnetUI;
+import m2105_ihm.ui.FenetreUI;
+import m2105_ihm.ui.PlanningUI;
+import m2105_ihm.ui.BoiteDialogUI;
+
+/**
+ *
+ * @author IUT2
+ */
+public class Controleur {
+    
+    /*
+     * Noyau Fonctionnel
+     */    
+    NoyauFonctionnel nf;
+            
+    /*
+     * Composants
+     */
+    private CarnetUI carnetUI;
+    private FenetreUI fenetre;
+    private PlanningUI planningUI;
+
+    /**
+     * Constructeur de la fenêtre principale
+     */
+    public Controleur() {
+        initUI();
+        initContent();
+    }
+    
+    /**
+     * Action créer un nouveau contact
+     */
+    public void creerContact() {
+        
+        /** TP5 : à compléter **/
+        
+    }
+
+    /**
+     * Action supprimer contact
+     */
+    public void supprimerContact() {
+        
+        /** TP5 : à compléter **/
+        
+    }
+    
+    /**
+     * Action créer un groupe de contacts
+     */
+    public void creerGroupe() {
+        
+        /** TP5 : à compléter **/
+              
+    }
+
+    /**
+     * Action supprimer un groupe de contacts
+     */
+    public void supprimerGroupe() {
+        
+        /** TP5 : à compléter **/
+        
+    }
+    
+    /**
+     * Crée un nouvel événement
+     */
+    public void creerEvenement() {
+    
+       /** Projet **/
+       
+    }
+
+    /**
+     * Supprime un événement existant
+     */
+    public void supprimerEvenement() {
+       
+       /** Projet **/
+       
+    }
+    
+    /**
+     * Ajouter un participant à un événement
+     */
+    public void ajouterParticipantEvenement() {
+    
+       /** Projet **/
+           
+    }
+
+    /**
+     * Retire un participant d'un événement
+     */
+    public void retirerParticipantEvenement() {
+    
+       /** Projet **/
+           
+    }
+
+    /**
+     * Met à jour la base de données
+     */
+    public void enregistrer() {
+        nf.updateDB();
+    }    
+        
+    /**
+     * Quitter l'application sans enregistrer les modifications
+     */
+    public void quitter() {
+        System.exit(0);
+    }
+
+    /**
+     * Création des composants constituant la fenêtre principale
+     */
+    private void initUI() {
+        /* Onglets */
+        carnetUI = new CarnetUI(this);
+        planningUI = new PlanningUI(this);
+
+        /* Fenêtre principale */
+        fenetre = new FenetreUI(this);
+        fenetre.addTab(carnetUI, "Carnet");     // onglet carnet
+        fenetre.addTab(planningUI, "Planning"); // onglet planning
+        fenetre.afficher();
+    }
+        
+    /**
+     * Alimente la liste avec la liste des contacts existants
+     */
+    private void initContent() {
+        nf = new NoyauFonctionnel();
+                
+        for(Contact c : nf.getContacts()) {
+            carnetUI.ajouterContact(c);
+        }
+        
+        for(GroupeContacts g : nf.getGroupes()) {
+            carnetUI.ajouterGroupe(g);
+        }
+        
+        for(Evenement e : nf.getEvenements()) {
+            planningUI.ajouterEvt(e);
+        }
+        
+        carnetUI.show();
+    }
+    
+    public void setContactSelected(boolean selected) {
+        fenetre.setMenuContactSelected(selected);
+        
+    }
+    
+    public void setEvtSelected(boolean selected) {
+        fenetre.setMenuEvenementSelected(selected);
+    }    
+}
---
+++ src/m2105_ihm/ui/FicheGroupeUI.java
@@ -0,0 +1,152 @@
+/*
+ * Module 2105 : module IHM : Carnet d'adresse
+ */
+package m2105_ihm.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import m2105_ihm.nf.Contact;
+import m2105_ihm.nf.GroupeContacts;
+
+/**
+ *
+ * @author IUT2
+ */
+public class FicheGroupeUI extends javax.swing.JPanel {
+    /*
+     * Composants graphiques constituants l'interface
+     */
+    
+    
+    
+    private CarnetUI     carnet;
+    private ZoneDessinUI zoneDessin;
+    
+    
+    
+    private JLabel labNomG;
+    private JTable tabContact;
+    private JTextField txNomG;
+    private JButton btEffacer;
+    public JButton btValider;
+    private DefaultTableModel model = new DefaultTableModel(); 
+    
+    /**
+     * Creates new form CarnetUI
+     */
+    public FicheGroupeUI(CarnetUI carnet) { 
+        super();
+       
+        this.carnet = carnet;
+        
+        initUIComponents();    
+        initListeners();
+    }
+
+    /**
+     * Crée et positionne les composants graphiques constituant l'interface
+     */
+    private void initUIComponents() {
+        
+        /** TP 2 : à compléter **/
+        
+        labNomG = new JLabel("Nom du groupe");
+        
+        txNomG = new JTextField(20);
+
+        String[] colonnes =  { "Nom", "Prenom" }; 
+        
+        model.setColumnIdentifiers(colonnes);
+        //String[] st = {"a","b"};
+        //model.addRow(st);
+        tabContact = new JTable(model);
+        
+        
+        btValider = new JButton("Valider");
+        btEffacer = new JButton("Effacer");
+        
+        zoneDessin = new ZoneDessinUI();
+        
+        
+        this.add(labNomG);
+        this.add(txNomG);
+        this.add(tabContact.getTableHeader());
+        this.add(tabContact);
+        
+        
+        
+        this.add(zoneDessin);
+        this.add(btValider);
+        this.add(btEffacer);
+                
+        /*this.add(new javax.swing.JButton("Hello !"));
+          this.add(new javax.swing.JLabel("Fiche groupe"));
+         */
+    }
+
+    /**
+     * Affecte des valeurs au formulaire de fiche groupe de contacts
+     * @param groupe groupe de contacts
+     * @return
+    */    
+    public boolean setValues(GroupeContacts groupe) {
+        if (groupe == null) {return false; }
+        
+        /** TP 2 : à compléter **/
+        String[] values = new String[2];
+        Contact[] ct = groupe.getContacts();
+        model.setRowCount(0);
+        for(int i =0; i<groupe.getContacts().length;i++) {
+            
+            values[0] = ct[i].getNom();
+            values[1] = ct[i].getPrenom();
+            model.addRow(values);
+        }
+        
+        txNomG.setText(groupe.getNom());
+        
+        return true;
+    }
+    
+    /**
+     * Retourne les valeurs associées au formulaire de fiche groupe de contacts
+     * @return
+     */    
+    public boolean getValues(GroupeContacts groupe) {
+        if (groupe == null) { return false; }
+        
+        /** TP 2 : à compléter **/
+        
+        /*
+         * Ne pas s'occuper des membres du groupe car traité via des
+         * commandes du menu qui appelera setValues
+         */
+        
+        groupe.setNom(txNomG.getText());
+        
+        
+        
+        return true;
+    }
+    
+    /**
+     * Initialise la gestion des événements
+     */
+    public void initListeners() {        
+        /*
+         * Réagit aux évènements produits par le bouton effacer
+         */
+        
+        btValider.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                carnet.setGroupeModified(true);
+            }
+            
+            
+        });
+    }    
+}
