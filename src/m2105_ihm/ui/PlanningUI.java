13d7d7f3a58b3e4abf963829f14f9c5c6bbc1997
souchoar <souchoar@pc-dg-033-12.iut2.upmf-grenoble.fr>
2015-06-03 14:54:37 +0200

    Ajout structure et test Calendrier

--- src/m2105_ihm/ui/PlanningUI.java
+++ src/m2105_ihm/ui/PlanningUI.java
@@ -41,23 +41,25 @@ public class PlanningUI extends JPanel {
     private JTextField                 calendar        = new CalendarUI();
     //private HashMap<Evenement,JButton> hashEvt         = new HashMap<>();
     
-    private JPanel      panelCorpsCalendar;
+    private JPanel      panelCorpsCalendar = new JPanel();
     //                      calendar
-    private JPanel          panelCorpsEvtSuivis;
-    private JPanel              panelEvtSuite;
-    private JPanel      panelCorpsInfo;
-    private JPanel          panelCorpsInfoEvt;
-    private JPanel              panelNomEvt;
-    private JPanel              panelDateEvt;
-    private JPanel              panelParticipants;
-    private JPanel                  panelListeParticipant;
-    private JPanel          panelDateActuelle;
+    private JPanel          panelCorpsEvt= new JPanel();
+    //private JPanel              panelEvtSuite;
+    private JPanel      panelCorpsInfo = new JPanel();
+    private JPanel          panelDateActuelle = new JPanel();
+    private JPanel          panelCorpsInfoEvt = new JPanel();
+    private JPanel              panelNomEvt = new JPanel();
+    private JPanel              panelDateEvt = new JPanel();
+    private JPanel              panelParticipants = new JPanel();
+    private JPanel                  panelListeParticipant = new JPanel();
+    private JPanel test = new Cal();
+
     
-    private JPanel panelCentreCorps  = new JPanel();
+    /*private JPanel panelCentreCorps  = new JPanel();
     private JPanel   panelDate    = new JPanel();
     private JPanel   panelGauche     = new JPanel();
     private JPanel   panelInfo    = new JPanel();
-    private JPanel   panelCorps   = new JPanel();
+    private JPanel   panelCorps   = new JPanel();*/
     
 
     /** 
@@ -87,104 +89,40 @@ public class PlanningUI extends JPanel {
         
 
         
-    panelCorpsCalendar= new JPanel();
-        //                      calendar
-        panelCorpsEvtSuivis= new JPanel();
-            panelEvtSuite= new JPanel();
-    panelCorpsInfo= new JPanel();
-        panelCorpsInfoEvt= new JPanel();
-            panelNomEvt= new JPanel();
-            panelDateEvt = new JPanel();
-            panelParticipants= new JPanel();
-                panelListeParticipant= new JPanel();
-        panelDateActuelle= new JPanel();
         
+    // this : BorderLayout
+    panelCorpsCalendar.setLayout(new BorderLayout());               // WEST (with preferedSize)
+        //      calendar                                                // NORTH
+        panelCorpsEvt.setLayout(new GridLayout());                      // CENTER
+            //JPanel              panelEvtSuite;                            // add
+    panelCorpsInfo.setLayout(new BorderLayout());                   // EAST
+        panelDateActuelle.setLayout(new BorderLayout());                // NORTH
+        panelCorpsInfoEvt.setLayout(new GridBagLayout());               // CENTER
+            panelNomEvt.setLayout(new BorderLayout());                      // add height = 1
+            panelDateEvt.setLayout(new BorderLayout());                     // add height = 1
+            panelParticipants.setLayout(new BorderLayout());                // add height = 3
+                panelListeParticipant.setLayout(new GridLayout());              // CENTER
         
-    panelCorpsCalendar.                 setLayout(new BorderLayout()); // WEST      of    this
-        //                              calendar                       // NORTH     of    panelCorpsCalendar
-        panelCorpsEvtSuivis.            setLayout(new GridLayout());   // CENTER    of    panelCorpsCalendar
-            //panelEvtSuite.              setLayout(new BorderLayout()); // Cellule   of    panelCorpsEvtSuivis
-    panelCorpsInfo.                     setLayout(new BorderLayout()); // EAST      of    this
-        panelNomEvt.                    setLayout(new GridLayout()); // 
-        panelCorpsInfoEvt.              setLayout(new BorderLayout()); // 
-        panelParticipants.              setLayout(new BorderLayout()); // 
-            panelListeParticipant.      setLayout(new GridLayout()); // 
-        panelDateActuelle.              setLayout(new BorderLayout()); // NORTH     of    panelCorpsInfo
+    test.setPreferredSize(new Dimension(200,200));
+    this.add(test);    
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
         
-        
-        
-        
-        /*panelCentreCorps  = new JPanel();
-        panelDate    = new JPanel();
-        panelGauche     = new JPanel();
-        panelInfo    = new JPanel();
-        panelCorps   = new JPanel();*/
-        
-        panelDate.   setLayout(new BorderLayout()); // NORTH
-        panelCorps.  setLayout(new BorderLayout()); // CENTER
-            panelInfo.   setLayout(new BorderLayout()); // NORTH
-            panelGauche.    setLayout(new GridLayout(20,1)); // CENTER
-            panelCentreCorps. setLayout(new GridLayout(20,1)); // WEST
-            
-        panelDate.   setBackground(Color.orange); // Rouge
-        panelCorps.  setBackground(Color.GREEN); // Vert
-            panelInfo.   setBackground(Color.lightGray); // Bleu
-            panelGauche.    setBackground(Color.YELLOW); // jaune
-            panelCentreCorps. setBackground(Color.PINK); // Rose
-        
-        
-        // Paneldate
-        
-        // PanelCentreCorps (participants)
-
-        panelCentreCorps.add(new JLabel("Participants : "));
-        for (int i = 0; i< 9;i++) {
-            panelCentreCorps.add(new JLabel("Particippant n°" + i));
-        }
-        panelCentreCorps.setPreferredSize(new Dimension(120,10));
-        
-        // PanelInfo (Info sur Evt
-        
-        panelInfo.add(new JLabel("Nom evenement : Anniv Machin"), BorderLayout.NORTH);
-        panelInfo.add(new JLabel("Date evenement : 18/06/2015"), BorderLayout.SOUTH);
-        panelInfo.setPreferredSize(new Dimension(10,50));
-        
-        
-        
-        // PanelCorps
-        
-        
-        
-        
-        panelGauche.add(new JLabel(" Choisir la date "));
-        panelGauche.add(calendar);
-        panelGauche.add(new JLabel("Evenenemets : "));
-        panelGauche.add(new JButton("Anniv Machin"));
-        panelGauche.setAutoscrolls(true);
-        panelGauche.add(new JButton("Anniv Truc"));
-        panelGauche.add(new JButton("Anniv Gus"));
-        panelGauche.add(new JButton("Anniv Machin"));
-        panelGauche.add(new JButton("Anniv Num"));
-        
-        // ADD
-        
-            panelCorps.add(panelInfo,BorderLayout.NORTH);
-            panelCorps.add(panelCentreCorps,BorderLayout.CENTER);
-        this.add(panelGauche,BorderLayout.WEST);
-        this.add(panelCorps, BorderLayout.CENTER);
-
-        
-
-        
-        
-        
-        
-        
-        
-        
-
-    }
-    
+    }  
     /**
      * Ajoute une entrée dans la liste de événements
      * @param title texte affiché dans la liste pour un contact
