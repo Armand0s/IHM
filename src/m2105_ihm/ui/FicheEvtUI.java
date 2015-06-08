88093932cdd564ec5b576259516dec59712cd051
souchoar <souchoar@pc-dg-039-02.iut2.upmf-grenoble.fr>
2015-06-08 09:55:39 +0200

    Creation

--- src/m2105_ihm/ui/FicheEvtUI.java
+++ src/m2105_ihm/ui/FicheEvtUI.java
@@ -3,6 +3,13 @@
  */
 package m2105_ihm.ui;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import m2105_ihm.nf.Mois;
 import m2105_ihm.nf.Contact;
 import m2105_ihm.nf.Evenement;
@@ -17,6 +24,13 @@ public class FicheEvtUI extends javax.swing.JPanel {
      * Attributs
      */
     private PlanningUI          planning;
+    
+    private JPanel          panelDateActuelle = new JPanel();
+    private JPanel          panelCorpsInfoEvt = new JPanel();
+    private JPanel              panelNomEvt = new JPanel();
+    private JPanel              panelDateEvt = new JPanel();
+    private JPanel              panelParticipants = new JPanel();
+    private JPanel                  panelListeParticipant = new JPanel();
     
     /**
      * Creates new form CarnetUI
@@ -35,7 +49,35 @@ public class FicheEvtUI extends javax.swing.JPanel {
      */
     private void initListeners() {
         
-        /** Projet : à compléter **/
+        panelDateActuelle.setLayout(new BorderLayout());                // NORTH
+        panelCorpsInfoEvt.setLayout(new GridBagLayout());               // CENTER
+            panelNomEvt.setLayout(new BorderLayout());                      // add height = 1
+            panelDateEvt.setLayout(new BorderLayout());                     // add height = 1
+            panelParticipants.setLayout(new BorderLayout());                // add height = 3
+                panelListeParticipant.setLayout(new GridLayout());              // CENTER     
+    this.setLayout(new BorderLayout());
+                
+    panelDateActuelle.add(new JButton("test"));
+    //panelCorpsInfoEvt.add(new JButton("test"));
+    panelNomEvt.add(new JButton("test"));
+    panelDateEvt.add(new JButton("test"));
+    //panelParticipants.setBackground(Color.red);
+    panelListeParticipant.add(new JButton("test"));
+        
+        GridBagConstraints c = new GridBagConstraints();
+        c.ipadx=105;
+        c.ipady=125;
+        c.gridx=1;
+    
+    
+    panelParticipants.add(new JLabel("Participants :"), BorderLayout.NORTH);
+                panelParticipants.add(panelListeParticipant, BorderLayout.CENTER);
+            panelCorpsInfoEvt.add(panelNomEvt,c);
+            panelCorpsInfoEvt.add(panelDateEvt,c);
+            c.ipady=250;
+            panelCorpsInfoEvt.add(panelParticipants,c);
+        this.add(panelDateActuelle, BorderLayout.NORTH);
+        this.add(panelCorpsInfoEvt, BorderLayout.CENTER);
 
     }
     
